1b33ea2e0bc65cb8be0949a89a68751539763f03
Cao quy Le <mikamokedo@gmail.com>
2023-09-07 00:23:08 +0700

    feat: input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import Message from "./components/chat/Message";
 import "./App.css";
+import Input from './components/chat/Input'
 
 function App() {
   return (
-    <div className="max-w-[800px] border border-black mt-[30px] mx-[auto] flex flex-col gap-[10px] p-[20px]">
+    <div className="max-w-[800px] border border-black mt-[30px] mx-[auto]">
+      <div className=" flex flex-col gap-[10px] p-[20px]">
       <Message
         textContent="just a messagee from sender ust a messagee from sender ust a messagee from sender just a messagee from sender ust a messagee from sender ust a messagee from sender"
         name="Q"
         isSender={true}
       />
       <Message textContent="just a messagee" name="phu123" isSender={false} />
+      </div>
+      <Input value="" onChange={() => null}/>
     </div>
   );
 }
---
+++ src/components/chat/Input.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+interface InputProps{
+    value:string;
+    onChange:( value:string) => void; 
+}
+const Input:React.FC<InputProps> = ({value,onChange}) => {
+  return (
+      <input value={value} onChange={(e) => onChange(e.target.value) }/>
+  )
+}
+
+export default Input
