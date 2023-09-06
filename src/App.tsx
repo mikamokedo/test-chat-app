17ce03907a275365cebc26402188dd3566d543f8
Cao quy Le <mikamokedo@gmail.com>
2023-09-07 03:26:59 +0700

    feat: load more message

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,40 @@ import SendButton from './components/chat/SendButton'
 import JoinRoomModal from './components/chat/JoinRoom'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from "./redux/store";
-import { updateInput,sendMessage,updateUserName } from './redux/slices/index'
-import { useState } from "react";
+import { updateInput,sendMessage,updateUserName, restoreMessage,IMessage } from './redux/slices/index'
+import { useEffect, useRef, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import {PAGE_SIZE} from './lib/const';
+
+
+const delay = () => new Promise((res) => setTimeout(res,1000));
+
 
 function App() {
+  const lassMessage = useRef<HTMLDivElement>(null)
+  const chatInner = useRef<HTMLDivElement>(null)
   const [isJoinRoom,setIsJoinRoom] = useState(false);
   const dispatch = useDispatch()
   const input = useSelector((state: RootState) => state.chat.input)
   const userName = useSelector((state: RootState) => state.chat.userName)
+  const StoresMessages = useSelector((state: RootState) => state.chat.messages);
+  const [page,setPage] = useState(1);
+  const [isLoading,setIsLoading] = useState(false);
+
+  const [currentMessages,setCurrentMessages] = useState<IMessage[]>([]);
+
+
   
   const handleSend = () =>{
-    dispatch(sendMessage({text:input,name:userName}));
+    dispatch(sendMessage({text:input,name:userName,id:uuidv4()}));
     dispatch(updateInput(""));
+    setTimeout(() =>{
+      if(lassMessage.current){
+        lassMessage.current.scrollIntoView({
+          behavior: 'smooth',
+        })
+      }
+    },300)
   }
   const handleJoinRoom = (name:string) =>{
     if(!name){
@@ -27,23 +49,118 @@ function App() {
     setIsJoinRoom(true);
   }
 
+
+
+  useEffect(() =>{
+    const timer = setInterval(() =>{
+        const oldMessages = window.localStorage.getItem("messages");
+        if(oldMessages){
+         const parseMess = JSON.parse(oldMessages);
+         const startIndex = parseMess.length - (page * PAGE_SIZE);
+         setCurrentMessages(() =>{
+          return parseMess.slice(
+            Math.max(startIndex, 0)
+          )
+        })   
+        }
+    },300)
+    return () => clearInterval(timer);
+
+  },[dispatch,currentMessages]);
+  
+  useEffect(() =>{
+    const oldMessages = window.localStorage.getItem("messages");
+    if(oldMessages){
+      const parseMess = JSON.parse(oldMessages);
+      const startIndex = parseMess.length - (page * PAGE_SIZE);
+      if(Array.isArray(parseMess)){
+        dispatch(restoreMessage(parseMess));
+        setCurrentMessages((state) =>{
+          return parseMess.slice(
+            Math.max(startIndex, 0),
+            startIndex + PAGE_SIZE
+          )
+        })
+      }
+    }
+  },[]);
+
+
+useEffect(() =>{
+  if(lassMessage.current){
+    lassMessage.current.scrollIntoView({
+      behavior: 'smooth',
+    })
+  }
+},[currentMessages.length,lassMessage,isJoinRoom])
+
+
+useEffect(() => {
+ const handleScroll = async() =>{
+    if(Math.ceil(StoresMessages.length / PAGE_SIZE) > page && !isLoading){
+      setIsLoading(true)
+      await delay();
+      setIsLoading(false)
+    }
+    
+ }
+  
+  if(chatInner.current) {   
+    chatInner.current.addEventListener('scroll',(e) =>{
+      if(chatInner.current?.scrollTop === 0 ){
+        handleScroll()
+      }
+    });  
+  }
+  return () => {
+    if(chatInner.current) {   
+      chatInner.current.removeEventListener('scroll', handleScroll);
+
+    }
+  };
+
+  
+}, [isJoinRoom,page,isLoading]);
+
+useEffect(() =>{
+  if(isLoading){
+    if(Math.ceil(StoresMessages.length / PAGE_SIZE) > page){
+      setPage((state) => state + 1);
+
+    }
+  }
+},[isLoading]);
+
+useEffect(() =>{
+  const startIndex = StoresMessages.length - (page * PAGE_SIZE);
+   setCurrentMessages((state) =>{
+      const newMess = StoresMessages.slice(
+        Math.max(startIndex, 0),
+        startIndex + PAGE_SIZE
+      ) 
+      return [...newMess,...state]
+    })
+
+},[page])
+
+
+// useEffect(() =>{
+//   const timer = setTimeout(() =>{
+//     if(lassMessage.current){
+//       lassMessage.current.scrollIntoView({
+//         behavior: 'smooth',
+//       })
+//     }
+//   },1000)
+//   return clearInterval(timer)
+// },[messages.length])
+
+
   return <>
     {!isJoinRoom ? <JoinRoomModal handleSend={handleJoinRoom} /> :     <div className="max-w-[800px] border border-black mt-[30px] mx-[auto]">
-    <div className=" flex flex-col gap-[10px] p-[20px]  max-h-[500px] overflow-y-scroll">
-    <Message
-      textContent="just a messagee from sender ust a messagee from sender ust a messagee from sender just a messagee from sender ust a messagee from sender ust a messagee from sender"
-      name="Q"
-      isSender={true}
-    />
-    <Message textContent="just a messagee" name="phu123" isSender={false} />
-    <Message textContent="just a messagee" name="phu123" isSender={false} />
-    <Message textContent="just a messagee" name="phu123" isSender={false} />
-    <Message textContent="just a messagee" name="phu123" isSender={false} />
-    <Message textContent="just a messagee" name="phu123" isSender={false} />
-    <Message textContent="just a messagee" name="phu123" isSender={false} />
-    <Message textContent="just a messagee" name="phu123" isSender={false} />
-    <Message textContent="just a messagee" name="phu123" isSender={false} />
-    <Message textContent="just a messagee" name="phu123" isSender={false} />
+    <div className=" flex flex-col gap-[10px] p-[20px]  max-h-[500px] overflow-y-scroll min-h-[500px]" ref={chatInner}>
+      {currentMessages.map((mess) => <Message textContent={mess.text} name={mess.name === userName ? "you" : mess.name} isSender={mess.name === userName} key={mess.id}/>)}
+      <div ref={lassMessage} className="opacity-0"/>
     </div>
     <div className="flex bg-indigo-500 p-[20px]">
     <Input value={input} onChange={(value) => dispatch(updateInput(value))}/>
--- src/redux/slices/index.ts
+++ src/redux/slices/index.ts
@@ -1,15 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
-interface Message {
+
+export interface IMessage {
   text:string;
   name: string;
+  id:string;
 }
 
 export interface ChatState {
-  messages:Message[];
+  messages:IMessage[];
   input:string;
   userName:string;
+
 }
 
 const initialState: ChatState = {
@@ -28,13 +31,18 @@ export const counterSlice = createSlice({
     updateUserName:(state, action: PayloadAction<string>) => {
       state.userName= action.payload
     },
-    sendMessage:(state, action: PayloadAction<Message>) => {
-      state.messages =  [...state.messages,action.payload]
-    }
+    sendMessage:(state, action: PayloadAction<IMessage>) => {
+      const messages = [...state.messages,action.payload]
+      state.messages =  messages;
+      window.localStorage.setItem("messages",JSON.stringify(messages))
+    },
+    restoreMessage :(state, action: PayloadAction<IMessage[]>) => {
+      state.messages =  action.payload;
+    },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { updateInput,sendMessage,updateUserName} = counterSlice.actions
+export const { updateInput,sendMessage,updateUserName,restoreMessage} = counterSlice.actions
 
 export default counterSlice.reducer
