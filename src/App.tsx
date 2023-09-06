9bc299e9723bda73a31d85bc5a0c8fe30a8368d8
Cao quy Le <mikamokedo@gmail.com>
2023-09-07 01:03:44 +0700

    feat: join room chat

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import Message from "./components/chat/Message";
 import "./App.css";
 import Input from './components/chat/Input'
 import SendButton from './components/chat/SendButton'
+import JoinRoomModal from './components/chat/JoinRoom'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from "./redux/store";
-import { updateInput,sendMessage } from './redux/slices/index'
+import { updateInput,sendMessage,updateUserName } from './redux/slices/index'
+import { useState } from "react";
 
 function App() {
+  const [isJoinRoom,setIsJoinRoom] = useState(false);
   const dispatch = useDispatch()
   const input = useSelector((state: RootState) => state.chat.input)
   const userName = useSelector((state: RootState) => state.chat.userName)
@@ -15,31 +18,39 @@ function App() {
     dispatch(sendMessage({text:input,name:userName}));
     dispatch(updateInput(""));
   }
+  const handleJoinRoom = (name:string) =>{
+    if(!name){
+      alert("Please input username first");
+      return;
+    }
+    dispatch(updateUserName(name))
+    setIsJoinRoom(true);
+  }
 
-  return (
-    <div className="max-w-[800px] border border-black mt-[30px] mx-[auto]">
-      <div className=" flex flex-col gap-[10px] p-[20px]  max-h-[500px] overflow-y-scroll">
-      <Message
-        textContent="just a messagee from sender ust a messagee from sender ust a messagee from sender just a messagee from sender ust a messagee from sender ust a messagee from sender"
-        name="Q"
-        isSender={true}
-      />
-      <Message textContent="just a messagee" name="phu123" isSender={false} />
-      <Message textContent="just a messagee" name="phu123" isSender={false} />
-      <Message textContent="just a messagee" name="phu123" isSender={false} />
-      <Message textContent="just a messagee" name="phu123" isSender={false} />
-      <Message textContent="just a messagee" name="phu123" isSender={false} />
-      <Message textContent="just a messagee" name="phu123" isSender={false} />
-      <Message textContent="just a messagee" name="phu123" isSender={false} />
-      <Message textContent="just a messagee" name="phu123" isSender={false} />
-      <Message textContent="just a messagee" name="phu123" isSender={false} />
-      </div>
-      <div className="flex bg-indigo-500 p-[20px]">
-      <Input value={input} onChange={(value) => dispatch(updateInput(value))}/>
-      <SendButton onSend={handleSend}/>
-      </div>
+  return <>
+    {!isJoinRoom ? <JoinRoomModal handleSend={handleJoinRoom} /> :     <div className="max-w-[800px] border border-black mt-[30px] mx-[auto]">
+    <div className=" flex flex-col gap-[10px] p-[20px]  max-h-[500px] overflow-y-scroll">
+    <Message
+      textContent="just a messagee from sender ust a messagee from sender ust a messagee from sender just a messagee from sender ust a messagee from sender ust a messagee from sender"
+      name="Q"
+      isSender={true}
+    />
+    <Message textContent="just a messagee" name="phu123" isSender={false} />
+    <Message textContent="just a messagee" name="phu123" isSender={false} />
+    <Message textContent="just a messagee" name="phu123" isSender={false} />
+    <Message textContent="just a messagee" name="phu123" isSender={false} />
+    <Message textContent="just a messagee" name="phu123" isSender={false} />
+    <Message textContent="just a messagee" name="phu123" isSender={false} />
+    <Message textContent="just a messagee" name="phu123" isSender={false} />
+    <Message textContent="just a messagee" name="phu123" isSender={false} />
+    <Message textContent="just a messagee" name="phu123" isSender={false} />
     </div>
-  );
+    <div className="flex bg-indigo-500 p-[20px]">
+    <Input value={input} onChange={(value) => dispatch(updateInput(value))}/>
+    <SendButton onSend={handleSend}/>
+    </div>
+  </div>}
+  </>
 }
 
 export default App;
---
+++ src/components/chat/JoinRoom.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Input from "./Input";
+import SendButton from "./SendButton";
+
+interface JoinRoomModalProps {
+    handleSend: (value:string) => void;
+}
+const JoinRoomModal: React.FC<JoinRoomModalProps> = ({handleSend}) => {
+  const [value, setValue] = useState("");
+  return (
+    <>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+              <h3 className="text-2xl font-semibold">
+                Please input user to join chat room
+              </h3>
+            </div>
+            {/*body*/}
+            <div className="relative p-6 flex-auto">
+              <div className="flex bg-indigo-500 p-[20px]">
+                <Input value={value} onChange={(e) => setValue(e)} />
+                <SendButton onSend={() => handleSend(value)} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </>
+  );
+};
+
+export default JoinRoomModal;
