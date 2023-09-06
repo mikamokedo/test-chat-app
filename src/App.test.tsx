ef59e7f817d809cf4acbb7189364f28062d854f3
Cao quy Le <mikamokedo@gmail.com>
2023-09-07 03:41:21 +0700

    feat: add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {getFirstChar} from './lib/chat'
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
+
+test("getFirstChar", () =>{
+
+  expect(getFirstChar("Quy")).toEqual("Q")
+  expect(getFirstChar("   Quy")).toEqual("Q")
+  expect(getFirstChar("123Quy")).toEqual("1")
+
+})
