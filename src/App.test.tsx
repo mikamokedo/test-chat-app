84bf0a5282f4eba6d869935d93ee033c8bc6f2b7
Cao quy Le <mikamokedo@gmail.com>
2023-09-07 04:02:12 +0700

    fix: error test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
 import {getFirstChar} from './lib/chat'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
 
 
@@ -16,4 +10,6 @@ test("getFirstChar", () =>{
   expect(getFirstChar("Quy")).toEqual("Q")
   expect(getFirstChar("   Quy")).toEqual("Q")
   expect(getFirstChar("123Quy")).toEqual("1")
+  expect(getFirstChar("  quy")).not.toEqual("")
+  expect(getFirstChar("  quy")).toEqual("Q")
 })
--- src/lib/chat.ts
+++ src/lib/chat.ts
@@ -1,3 +1,3 @@
 export const getFirstChar = (name:string) => {
-    return  name.split(' ').join('').substring(0,1);
+    return  name.split(' ').join('').substring(0,1).toLocaleUpperCase();
 }
