638f6b506d35769e54fb513d9642ce5116960c07
Cao quy Le <mikamokedo@gmail.com>
2023-09-07 20:02:31 +0700

    feat: focus on input

--- src/components/chat/Input.tsx
+++ src/components/chat/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useRef,useEffect} from 'react'
 
 
 interface InputProps{
@@ -6,8 +6,13 @@ interface InputProps{
     onChange:( value:string) => void; 
 }
 const Input:React.FC<InputProps> = ({value,onChange}) => {
+    const ref = useRef<HTMLInputElement>(null);
+
+    useEffect(() =>{
+      ref.current?.focus()
+    },[ref])
   return (
-      <input value={value} onChange={(e) => onChange(e.target.value) } className='rounded-[10px] w-full p-[5px]'/>
+      <input value={value} onChange={(e) => onChange(e.target.value) } className='rounded-[10px] w-full p-[5px]' ref={ref}/>
   )
 }
 
