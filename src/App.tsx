bef3928a00ab42331d91d21f13ed9bd5df9404f1
Cao quy Le <mikamokedo@gmail.com>
2023-09-08 12:19:50 +0700

    fix: bug

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {
   restoreMessage,
   IMessage,
 } from "./redux/slices/index";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { PAGE_SIZE } from "./lib/const";
 import { useKeyPress } from "./lib/useKeyPress";
@@ -19,32 +19,31 @@ import { useKeyPress } from "./lib/useKeyPress";
 const delay = () => new Promise((res) => setTimeout(res, 1000));
 
 function App() {
-  const lassMessage = useRef<HTMLDivElement>(null);
-  const chatInner = useRef<HTMLDivElement>(null);
-  const [isJoinRoom, setIsJoinRoom] = useState(false);
   const dispatch = useDispatch();
-  const [input,setInput] = useState("");
   const userName = useSelector((state: RootState) => state.chat.userName);
   const storesMessages = useSelector((state: RootState) => state.chat.messages);
+
+  const lassMessage = useRef<HTMLDivElement>(null);
+  const chatInner = useRef<HTMLDivElement>(null);
+
+  const [isJoinRoom, setIsJoinRoom] = useState(false);
+  const [input, setInput] = useState("");
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [showToBottom, setShowToBottom] = useState(false);
   const [currentMessages, setCurrentMessages] = useState<IMessage[]>([]);
 
-  const handleSend = () => {
+  const handleSend = useCallback(() => {
     if (!input) {
       return;
     }
     dispatch(sendMessage({ text: input, name: userName, id: uuidv4() }));
-   setInput("");
+    setInput("");
     setTimeout(() => {
-      if (lassMessage.current) {
-        lassMessage.current.scrollIntoView({
-          behavior: "smooth",
-        });
-      }
-    }, 300);
-  };
+      scrollToBottom();
+    }, 200);
+  }, [dispatch, input, userName]);
+
   const handleJoinRoom = (name: string) => {
     if (!name) {
       alert("Please input username first");
@@ -54,34 +53,41 @@ function App() {
     setIsJoinRoom(true);
   };
 
+  const scrollToBottom = () => {
+    if (lassMessage.current) {
+      lassMessage.current.scrollIntoView();
+    }
+  };
+
   useEffect(() => {
     const timer = setInterval(() => {
       const oldMessages = window.localStorage.getItem("messages");
       const parseMess = JSON.parse(oldMessages ?? "");
-      if (parseMess) {
-          const startIndex = parseMess.length - page * PAGE_SIZE;
-          setCurrentMessages(() => {
-            return parseMess.slice(Math.max(startIndex, 0));
-          });
+      if (
+        parseMess &&
+        currentMessages.length &&
+        parseMess[parseMess.length - 1]?.id !==
+          currentMessages[currentMessages.length - 1]?.id
+      ) {
+        const startIndex = parseMess.length - page * PAGE_SIZE;
+        setCurrentMessages(() => {
+          return parseMess.slice(Math.max(startIndex, 0));
+        });
+        setTimeout(() => {
+          scrollToBottom();
+        }, 200);
       }
     }, 200);
     return () => clearInterval(timer);
-  }, [dispatch,storesMessages,page]);
+  }, [dispatch, storesMessages, page, currentMessages]);
 
   useEffect(() => {
     const oldMessages = window.localStorage.getItem("messages");
     const parseMess = JSON.parse(oldMessages ?? "");
     if (parseMess) {
-      const startIndex = parseMess.length - page * PAGE_SIZE;
-        dispatch(restoreMessage(parseMess));
-        setCurrentMessages((state) => {
-          return parseMess.slice(
-            Math.max(startIndex, 0),
-            startIndex + PAGE_SIZE
-          );
-        });
+      dispatch(restoreMessage(parseMess));
     }
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
     scrollToBottom();
@@ -89,66 +95,68 @@ function App() {
 
   useEffect(() => {
     const handleScroll = async () => {
-      if (storesMessages.length / PAGE_SIZE > page && !isLoading) {
-        setIsLoading(true);
-        await delay();
-        setIsLoading(false);
-      }
+      setIsLoading(true);
+      await delay();
+      setIsLoading(false);
     };
+
     if (chatInner.current) {
       chatInner.current.addEventListener("scroll", (e) => {
-        if(!chatInner.current){
+        if (!chatInner.current) {
           return;
         }
         if (chatInner.current?.scrollTop === 0) {
           handleScroll();
         }
-        if(chatInner.current.scrollHeight - chatInner.current?.scrollTop > chatInner.current?.clientHeight + 400){
+        if (
+          chatInner.current.scrollHeight - chatInner.current?.scrollTop >
+          chatInner.current?.clientHeight + 400
+        ) {
           setShowToBottom(true);
-        }else{
+        } else {
           setShowToBottom(false);
         }
       });
     }
+
     return () => {
       if (chatInner.current) {
         chatInner.current.removeEventListener("scroll", handleScroll);
       }
     };
-  }, [isJoinRoom, page, isLoading,storesMessages]);
+  }, [isJoinRoom, page, isLoading, storesMessages]);
 
   useEffect(() => {
-    if (isLoading) {
-        setPage((state) => state + 1);
+    if (isLoading && storesMessages.length / PAGE_SIZE > page) {
+      setPage((state) => state + 1);
     }
   }, [isLoading]);
 
   useEffect(() => {
     const startIndex = storesMessages.length - page * PAGE_SIZE;
-    setCurrentMessages((state) => {
+    setCurrentMessages(() => {
       return storesMessages.slice(Math.max(startIndex, 0));
     });
-  }, [page]);
+  }, [page, storesMessages]);
 
   const pressed = useKeyPress("Enter");
   useEffect(() => {
     if (pressed && isJoinRoom) {
       handleSend();
     }
-  }, [pressed,isJoinRoom]);
+  }, [pressed, isJoinRoom, handleSend]);
 
-  const scrollToBottom = () => {
-    if (lassMessage.current) {
-      lassMessage.current.scrollIntoView();
-    }
-  };
-  
   return (
     <>
       {!isJoinRoom ? (
         <JoinRoomModal handleSend={handleJoinRoom} />
       ) : (
         <div className="max-w-[800px] border border-black mt-[30px] mx-[auto] relative">
+          {isLoading && (
+            <div className="w-full text-sm text-center absolute top-[0px] left-[0] cursor-pointer bg-gray-100 py-2 z-10">
+              Get more message
+            </div>
+          )}
           <div
             className=" flex flex-col gap-[10px] p-[20px]  max-h-[500px] overflow-y-scroll min-h-[500px]"
             ref={chatInner}
@@ -164,10 +172,7 @@ function App() {
             <div ref={lassMessage} className="opacity-0" />
           </div>
           <div className="flex bg-indigo-500 p-[20px]">
-            <Input
-              value={input}
-              onChange={setInput}
-            />
+            <Input value={input} onChange={setInput} />
             <SendButton onSend={handleSend} />
           </div>
           {showToBottom && (
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import chatReducer from '../redux/slices/index'
-import { setupListeners } from '@reduxjs/toolkit/query'
-import { pokemonApi } from '../services/index'
 
 export const store = configureStore({
   reducer: {
     chat: chatReducer,
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(pokemonApi.middleware),
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
-setupListeners(store.dispatch)
