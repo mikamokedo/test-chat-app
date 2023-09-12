067d89c385a5a4c5665b4632b930d9c5e76a0144
Cao quy Le <mikamokedo@gmail.com>
2023-09-12 17:59:57 +0700

    fix: update test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,14 +2,34 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import {getFirstChar} from './lib/chat'
 
+describe('getFirstChar function', () => {
+  it('should return the first character in uppercase from a single word name', () => {
+    const result = getFirstChar('John');
+    expect(result).toBe('J');
+  });
 
+  it('should return the first character in uppercase from a multiple word name', () => {
+    const result = getFirstChar('Alice Johnson');
+    expect(result).toBe('A');
+  });
 
+  it('should handle leading and trailing spaces', () => {
+    const result = getFirstChar('   Alex Smith   ');
+    expect(result).toBe('A');
+  });
 
+  it('should return an empty string for an empty input', () => {
+    const result = getFirstChar('');
+    expect(result).toBe('');
+  });
 
-test("getFirstChar", () =>{
-  expect(getFirstChar("Quy")).toEqual("Q")
-  expect(getFirstChar("   Quy")).toEqual("Q")
-  expect(getFirstChar("123Quy")).toEqual("1")
-  expect(getFirstChar("  quy")).not.toEqual("")
-  expect(getFirstChar("  quy")).toEqual("Q")
-})
+  it('should return an empty string for input with only spaces', () => {
+    const result = getFirstChar('    ');
+    expect(result).toBe('');
+  });
+
+  it('should handle special characters and symbols', () => {
+    const result = getFirstChar('@#$% Name123');
+    expect(result).toBe('N');
+  });
+});
--- src/lib/chat.ts
+++ src/lib/chat.ts
@@ -1,3 +1,3 @@
 export const getFirstChar = (name:string) => {
-    return  name.split(' ').join('').substring(0,1).toLocaleUpperCase();
+    return  name.replace(/[^a-zA-Z0-9 ]/g, '').split(' ').join('').substring(0,1).toLocaleUpperCase();
 }
